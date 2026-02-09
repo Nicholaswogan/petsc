499f4311e5f045898b10287abe99f54292b85479
Nick Wogan <nicholaswogan@gmail.com>
2026-02-09 09:37:53 -0800

    remove build

--- fortran/build/CMakeFiles/tspseudo.dir/cmake_clean_Fortran.cmake
+++
@@ -1,6 +0,0 @@
-# Remove fortran modules provided by this target.
-FILE(REMOVE
-  "pseudo_transient.mod"
-  "PSEUDO_TRANSIENT.mod"
-  "CMakeFiles/tspseudo.dir/pseudo_transient.mod.stamp"
-  )
--- fortran/build/CMakeFiles/tspseudo.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libtspseudo.a"
-)
